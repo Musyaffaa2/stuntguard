8b74f87193bc49b30b792399d7bc206e46f87986
azrappp <118091170+azrappp@users.noreply.github.com>
2024-05-31 19:51:29 +0700

    Adding Layout for login page

--- app/src/main/java/com/example/stuntguard/MainActivity.java
+++ app/src/main/java/com/example/stuntguard/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.stuntguard;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.layout_login);
+
+        EditText usernameEditText = (EditText) findViewById(R.id.editTextusername);
+        usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String currentText = usernameEditText.getText().toString();
+                if (currentText.equals("Username") && currentText.isEmpty()) {
+                    usernameEditText.setText("");
+                    usernameEditText.setTextColor(Color.parseColor("#FFFFFF"));
+                }
+            }
         });
+
+
+
     }
+
+
+
 }
