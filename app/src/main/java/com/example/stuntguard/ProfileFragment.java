75bf18283b3103b6beef5ec10954bb492bb4b741
Musyaffaa2 <musyaffaarwin@student.ub.ac.id>
2024-06-09 18:39:29 +0700

    loginregisterlogout

--- app/src/main/java/com/example/stuntguard/ProfileFragment.java
+++ app/src/main/java/com/example/stuntguard/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.stuntguard;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
 
+    private FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button logoutButton = view.findViewById(R.id.buttonLogout);
+        logoutButton.setOnClickListener(v -> {
+            mAuth.signOut();
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+        });
+
+        return view;
     }
 }
